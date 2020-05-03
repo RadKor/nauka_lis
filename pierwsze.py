3751a8af32511d1911dcc22ad9c597e25c0a9b5e
RadKor <metalium@wp.pl>
2020-05-03 15:56:07 +0200

    pierwsze_liczby

---
+++ pierwsze.py
@@ -0,0 +1,16 @@
+
+liczba = int(input('Podaj liczbe: '))
+
+def check_pierwsza(liczba):
+    if liczba > 1:
+
+        for i in range(2, liczba):
+            if liczba % i == 0:
+                return False
+            else:
+                return True
+            
+    else:
+        return False
+
+print(check_pierwsza(liczba))
